158dad86fd2020b627fda20f931a1dd799305adc
BarmaIey <Dakos4k@gmail.com>
2026-03-16 13:01:19 +0000

    Added string_task.c, changed main.c and run argument

--- src/main.c
+++ src/main.c
@@ -1,8 +1,22 @@
-/* hello.c */
 #include <stdio.h>
+#include <string.h>
 
-void main (void)
-{
-	printf ("Hello World\n");
-	printf ("Goodbye World\n");
+void insert_ascii_codes(char *input, char *output);
+
+int main(int argc, char *argv[]) {
+		char input[200];
+		char output[600];
+
+		if (argc > 1) {
+				strncpy(input, argv[1], sizeof(input) - 1);
+				input[sizeof(input) - 1] = '\0';
+		} else {
+				printf("Enter string: ");
+				fgets(input, sizeof(input), stdin);
+				input[strcspn(input, "\n")] = 0; 
+		}
+
+		insert_ascii_codes(input, output);
+		printf("Result: %s\n", output);
+		return 0;
 }
---
+++ src/string_task.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void insert_ascii_codes(char *input, char *output)
+{
+        int i = 0, j = 0;
+        while (input[i] != '\0')
+        {
+                output[j++] = input[i];
+
+                int code = (int)input[i];
+                j += sprintf(&output[j], "%d", code);
+
+                i++;
+        }
+
+        output[j] = '\0';
+}
